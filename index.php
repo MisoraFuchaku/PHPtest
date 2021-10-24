09e5bb57b9dd3ed601128b6a0f1e7b3e70a723ec
MisoraFuchaku <89375573+MisoraFuchaku@users.noreply.github.com>
2021-10-24 14:14:26 +0900

    prime number

--- index.php
+++ index.php
@@ -12,3 +12,17 @@ for($i = 1; $i <= 100; $i++){
     }
         echo '<br>';
 }
+
+//2.素数算出
+for($i=1; $i<1000; $i++){
+    $yakusuu=0;
+      for($j=1; $j<=$i; $j++){
+          if($i%$j==0){
+              $yakusuu++;
+          }
+      }
+    if($yakusuu==2){
+        print $i."\n";
+        echo '<br>';
+    }
+}
